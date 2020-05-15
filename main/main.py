0c78732851f2f2099605c6b68868d2ca6b62ea8c
Junpeng He <hejunpeng2012@hotmail.com>
2020-05-15 21:16:31 +0800

    fix macOS os error in start script

--- main/main.py
+++ main/main.py
@@ -8,12 +8,7 @@ def start_web():
     start_test_server()
 
 
-def start_app():
-    messaging_process = Process(target=start_messaging)
-    web_process = Process(target=start_web)
-    messaging_process.start()
-    web_process.start()
-
-
 if __name__ == '__main__':
-    start_app()
+    web_process = Process(target=start_web)
+    web_process.start()
+    start_messaging()
