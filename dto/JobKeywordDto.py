3d121b872dd544d4253900347a9fc5e928163f33
hejp009 <hejunpeng2012@hotmail.com>
2020-06-03 18:23:47 +0800

    add category set to JobKeywordDto

--- dto/JobKeywordDto.py
+++ dto/JobKeywordDto.py
@@ -1,8 +1,8 @@
-from util.json_util import jsonify
+import json
+
 from util.str_util import to_str
 
 
-@jsonify
 @to_str
 class JobKeywordDTO:
     def __init__(self, job_id: str = None, request_id: str = None, keyword_list: list = None):
@@ -10,7 +10,14 @@ class JobKeywordDTO:
             keyword_list = []
         self.job_id = job_id
         self.keyword_list = keyword_list
+        self.category_list = set()
         self.request_id = request_id
 
     def add_keyword(self, keyword_dict: dict):
         self.keyword_list.append(keyword_dict)
+        self.category_list.add(keyword_dict["category"])
+
+    def to_json(self):
+        """ custom jsonify method, because the set class is not json serializable """
+        self.category_list = list(self.category_list)
+        return json.dumps(self.__dict__)
