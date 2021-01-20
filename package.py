73c56e284f7710a76058fedc6c671fdad45e9beb
Junpeng He <hejunpeng2012@hotmail.com>
2021-01-20 20:39:36 +0800

    refactor(config): redis config refactor

--- package.py
+++ package.py
@@ -1,9 +1,12 @@
 if __name__ == '__main__':
     import os
-    import socket
 
     image_name = "job-keyword"
-    docker_tag = f"{socket.gethostbyname('alaitp-cloud')}:5555/{image_name}"
+    # docker_tag = f"{socket.gethostbyname('alaitp-cloud')}:5555/{image_name}:dev"
+    docker_tag = f"localhost:5555/{image_name}:local-dev"
     os.system(f"docker build --tag={image_name} --force-rm=true .")
     os.system(f"docker tag {image_name} {docker_tag}")
     os.system(f"docker push {docker_tag}")
+
+# kubectl delete deploy job-keyword
+# kubectl create deploy job-keyword --image=localhost:5555/job-keyword:local-dev
--- src/config/redis_config.py
+++ src/config/redis_config.py
@@ -16,11 +16,11 @@ class RedisTemplate:
 
     def __init__(self):
         log.info(f"connecting redis using url: {REDIS_URL}")
-        self.co_occurrence = redis.ConnectionPool.from_url(url=REDIS_URL, db=0)
-        self.job_search_pool = redis.ConnectionPool.from_url(url=REDIS_URL, db=1)
-        self.job_keyword_pool = redis.ConnectionPool.from_url(url=REDIS_URL, db=2)
-        self.standard_word_pool = redis.ConnectionPool.from_url(url=REDIS_URL, db=3)
-        self.standard_category_pool = redis.ConnectionPool.from_url(url=REDIS_URL, db=4)
+        self.co_occurrence = redis.ConnectionPool.from_url(url=REDIS_URL + "/0")
+        self.job_search_pool = redis.ConnectionPool.from_url(url=REDIS_URL + "/1")
+        self.job_keyword_pool = redis.ConnectionPool.from_url(url=REDIS_URL + "/2")
+        self.standard_word_pool = redis.ConnectionPool.from_url(url=REDIS_URL + "/3")
+        self.standard_category_pool = redis.ConnectionPool.from_url(url=REDIS_URL + "/4")
 
     def db(self, db: int) -> Redis:
         if db == 0:
