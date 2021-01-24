a752670334c538f80a39462088a5fd5b887f4568
Junpeng He <hejunpeng2012@hotmail.com>
2021-01-24 16:28:17 +0800

    build(script): pro deploy script

--- package.py
+++ package.py
@@ -1,12 +1,68 @@
+import os
+import sys
+
+
+app_name = "job-keyword"
+docker_tag = app_name + ":" + "prod"
+
+def git_pull():
+    run_cmd("git pull")
+
+def change_config_file():
+    print "changing config file"
+
+    bootstrap_yml = open("src/main/resources/bootstrap.yml", "w")
+    bootstrap_yml.truncate()
+
+    env = sys.argv[1] if len(sys.argv) > 1 else "dev"
+
+    if env == "dev":
+        env_yml = "bootstrap-dev.yml"
+    elif env == "test":
+        env_yml = "bootstrap-test.yml"
+    elif env == "prod":
+        env_yml = "bootstrap-prod.yml"
+
+    env_yml_path = "src/main/resources/" + env_yml
+    env_yml_file = open(env_yml_path)
+
+    for line in env_yml_file:
+        bootstrap_yml.write(line)
+
+    bootstrap_yml.close()
+    env_yml_file.close()
+
+    print "using " + env_yml_path + " as config file"
+
+def package_jar():
+    run_cmd("mvn clean")
+    run_cmd("mvn package -Dmaven.test.skip=true")
+
+def build_image():
+    print_cmd("eval $(minikube docker-env)")
+    print_cmd("docker build --tag=" + docker_tag + " --force-rm=true .")
+    print_cmd("eval $(minikube docker-env -u)")
+
+def k8s_deploy():
+    print_cmd("kubectl delete deployment " + app_name)
+    print_cmd("kubectl create deployment " + app_name + " --image=" + docker_tag)
+
+
+def run_sudo_cmd(cmd):
+    cmd = "sudo " + cmd
+    print cmd
+    os.system(cmd)
+
+def run_cmd(cmd):
+    print cmd
+    os.system(cmd)
+
+def print_cmd(cmd):
+    print cmd
+
 if __name__ == '__main__':
-    import os
-
-    image_name = "job-keyword"
-    # docker_tag = f"{socket.gethostbyname('alaitp-cloud')}:5555/{image_name}:dev"
-    docker_tag = f"localhost:5555/{image_name}:local-dev"
-    os.system(f"docker build --tag={image_name} --force-rm=true .")
-    os.system(f"docker tag {image_name} {docker_tag}")
-    os.system(f"docker push {docker_tag}")
-
-# kubectl delete deploy job-keyword
-# kubectl create deploy job-keyword --image=localhost:5555/job-keyword:local-dev
+    git_pull()
+    change_config_file()
+    package_jar()
+    build_image()
+    k8s_deploy()
--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,8 @@
 
-RABBITMQ_URL = f"amqp://{'junpeng'}:{'921102'}@{'localhost:5672'}/%2F"
-REDIS_URL = "redis://localhost:16379"
-SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'localhost:5432'}"
+# RABBITMQ_URL = f"amqp://{'junpeng'}:{'921102'}@{'localhost:5672'}/%2F"
+# REDIS_URL = "redis://localhost:16379"
+# SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'localhost:5432'}"
+
+RABBITMQ_URL = f"amqp://{'junpeng'}:{'921102'}@{'external-rabbitmq'}/%2F"
+REDIS_URL = "redis://external-redis"
+SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'external-psql'}"
