06596e176e2ec5a6f88b90c5bd6f23c80b12a926
Junpeng He <hejunpeng2012@hotmail.com>
2021-02-04 20:39:24 +0800

    config(psql): update psql config

--- src/config/config.py
+++ src/config/config.py
@@ -3,6 +3,6 @@
 # REDIS_URL = "redis://localhost:16379"
 # SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'localhost:5432'}"
 
-RABBITMQ_URL = f"amqp://{'junpeng'}:{'921102'}@{'rabbitmq'}/%2F"
+RABBITMQ_URL = f"amqp://{'hejunpeng'}:{'921102'}@{'rabbitmq'}/%2F"
 REDIS_URL = "redis://redis"
-SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'external-psql'}"
+SQL_URL = f"postgresql://{'hejunpeng'}:{'921102'}@{'psql'}"
